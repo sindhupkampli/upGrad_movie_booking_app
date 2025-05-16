e8883a71f736f0bb83afd9360edba8140d920545
sindhupkampli <sindhupkampli@gmail.com>
2025-05-16 21:18:11 +0530

    Update genre.controller.js

--- controllers/genre.controller.js
+++ controllers/genre.controller.js
@@ -3,12 +3,11 @@ const Genre = require('../models/genre.model');
 
 
 exports.findAllGenres = async (req, res) => {
-  console.log("--------------------")
-  try {
+   try {
     const genres = await Genre.find(); 
     res.status(200).json(genres);
   } catch (error) {
     console.error('Error finding genres:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: 'Server Encountered an error' });
   }
 };
