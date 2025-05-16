eb47cdd454373652898921d399ed4b22a1d16ec2
sindhupkampli <sindhupkampli@gmail.com>
2025-05-16 21:17:32 +0530

    Update artist.controller.js

--- controllers/artist.controller.js
+++ controllers/artist.controller.js
@@ -4,10 +4,10 @@ const Artist = require('../models/artist.model');
 
 exports.findAllArtists = async (req, res) => {
   try {
-    const artists = await Artist.find(); // Finding all artists
+    const artists = await Artist.find(); 
     res.status(200).json(artists);
   } catch (error) {
     console.error('Error finding artists:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: 'Server encountered an error' });
   }
 };
