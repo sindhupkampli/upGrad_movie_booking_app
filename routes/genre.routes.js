fa050a89ff916d407d8724fc8c2dd6119dec2092
sindhupkampli <sindhupkampli@gmail.com>
2025-05-16 21:13:42 +0530

    Create genre.routes.js

---
+++ routes/genre.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const genreController = require('../controllers/genre.controller');
+
+
+router.get('/', genreController.findAllGenres);
+
+module.exports = router;
